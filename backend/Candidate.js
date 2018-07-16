e474ab46af61d60121385891d6a3edf263fe9c69
Rashid Elhouli <rashidelhouli@gmail.com>
2018-07-16 13:04:04 +1000

    Add schema for candidate and admin

---
+++ backend/Candidate.js
@@ -0,0 +1,34 @@
+const mongoose = require('../db/connectdb')
+const {Schema} = mongoose
+
+const candidateSchema = new Schema ({
+    
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    emailAddress: String,
+    location: String,
+    currentJobTitle: String,
+    CurrentEmployer: String,
+    roleType: String,
+    responsibilities: String,
+    technologies: [String],
+    minSalary: Number,
+    expectedJobTitle: String,
+    techStack: [String],
+    contactSource: String,
+    clientNotes: String,
+    personalNotes: String, 
+    priority: [String],
+    expectedCompany: [String],
+    resumeUrl: String,
+    isActive: Boolean,
+    createdAt: Date,
+    updatedAt: Date
+
+})
+
+const Candidate = mongoose.model('Candidates', candidateSchema)
+
+module.exports = Candidate
+
