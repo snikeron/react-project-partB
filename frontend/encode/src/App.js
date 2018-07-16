9a5aa1f8557dd3ae550cabfdf59cf4364d1d0b3a
Ashley <36495929+myashley@users.noreply.github.com>
2018-07-16 16:22:31 +1000

    Initial form structure

--- frontend/encode/src/App.js
+++ frontend/encode/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CandidateForm from './form/CandidateForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <CandidateForm />
       </div>
     );
   }
---
+++ frontend/encode/src/form/CandidateForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import StepZilla from 'react-stepzilla'
+import Step1 from './steps/Step1'
+import Step2 from './steps/Step2'
+import Step3 from './steps/Step3'
+import Step4 from './steps/Step4'
+import Step5 from './steps/Step5'
+import Step6 from './steps/Step6'
+
+export default class CandidateForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+  
+      this.candidateData = {
+        savedToCloud: false
+      };
+    }
+  
+    componentDidMount() {}
+  
+    getData() {
+      return this.candidateData;
+    }
+  
+    updateData(update) {
+      this.candidateData = {
+        ...this.candidateData,
+        ...update,
+      }
+    }
+  
+    render() {
+      const steps =
+      [
+        {name: 'Step1', component: <Step1 getData={() => (this.getData())} updateData={(u) => {this.updateData(u)}} />},
+        {name: 'Step2', component: <Step2 getData={() => (this.getData())} updateData={(u) => {this.updateData(u)}} />},
+        {name: 'Step3', component: <Step3 getData={() => (this.getData())} updateData={(u) => {this.updateData(u)}} />},
+        {name: 'step4', component: <Step4 getData={() => (this.getData())} updateData={(u) => {this.updateData(u)}} />},
+        {name: 'Step5', component: <Step5 getData={() => (this.getData())} updateData={(u) => {this.updateData(u)}} />},
+        {name: 'Step6', component: <Step6 getData={() => (this.getData())} updateData={(u) => {this.updateData(u)}} />}
+      ]
+  
+      return (
+        <div className='candidateForm'>
+          <div className='step-progress'>
+            <StepZilla
+              steps={steps}
+              preventEnterSubmission={true}
+              nextTextOnFinalActionStep={"Save"}
+            //   hocValidationAppliedTo={[3]}
+              startAtStep={window.sessionStorage.getItem('step') ? parseFloat(window.sessionStorage.getItem('step')) : 0}
+              onStepChange={(step) => window.sessionStorage.setItem('step', step)}
+             />
+          </div>
+        </div>
+      )
+    }
+  }
+  
