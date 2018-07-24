5e448b10477ae95b7c6f9a011eb6f373bf7f355a
Rashid Elhouli <rashidelhouli@gmail.com>
2018-07-24 15:14:00 +1000

    Add candidate view

--- frontend/encode-admin/src/App.js
+++ frontend/encode-admin/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import CandidateList from './Components/CandidateList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import CandidateInfo from './Components/CandidateInfo'
 
 class App extends Component {
   render() {
@@ -13,8 +15,17 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-          <CandidateList/>
-        
+        <main className="main-wrapper">
+          <Router>
+            <Switch>
+              <Route exact path="/candidates" component={CandidateList} />
+              <Route path="/candidates/:id" render={({ match }) => {
+                const { id } = match.params
+                return <CandidateInfo _id={id} />
+              }} />
+            </Switch>
+          </Router>
+        </main>
       </div>
     );
   }
