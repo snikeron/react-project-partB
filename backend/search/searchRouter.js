8e20cf5098ea82856e4492a30da47ef2efd6bca5
Ashley <36495929+myashley@users.noreply.github.com>
2018-07-26 01:01:56 +1000

    Test search options on search routes

--- backend/search/searchRouter.js
+++ backend/search/searchRouter.js
@@ -7,7 +7,7 @@ router.post('/', (req, res) => {
     console.log(req.body)
     Candidate.find({
         $text: {
-            $search: new RegExp(req.body.query)
+            $search: new RegExp(req.body.keyword)
         }
     })
         .then(results => {
@@ -17,5 +17,80 @@ router.post('/', (req, res) => {
             throw new Error(err.message)
         })
 })
+
+//// Find Model
+router.post('/salary', (req, res) => {
+    Candidate.find({
+        minSalary: { $gte: req.body.minSalary },
+        
+    })
+        .then(results => {
+            res.status(200).json(results)
+        })
+})
+
+router.post('/test', (req, res) => {
+
+    console.log(req.body)
+    console.log(Object.keys(req.body).length)
+    console.log(Object.keys(req.body))
+    console.log(Object.values(req.body))
+    console.log(Object.keys(req.body)[0])
+    console.log(Object.values(req.body)[0])
+
+    Candidate.find({
+        minSalary: { $gte: req.body.minSalary },
+        location: req.body.location,
+     
+    })
+    .then(results => {
+            res.status(200).json(results)
+        })
+
+    // switch(Object.keys(req.body).length) {
+    //     case 0:
+            
+    //         break;
+    //     case 1:
+    //         console.log('case1')
+
+    //         const key1 = Object.keys(req.body)[0]
+    //         const value1 = Object.values(req.body)[0]
+
+    //         console.log(key1, value1)
+
+    //         Candidate.find({
+    //             minSalary: req.body.minSalary,
+    //         })
+    //         .then(results => {
+    //             res.status(200).json(results)
+    //         })
+            
+    //         break;
+    //     case 2: 
+
+    //         Candidate.find({
+    //             minSalary: { $gte: req.body.minSalary },
+    //             location: req.body.location,
+    //         })
+    //         .then(results => {
+    //             res.status(200).json(results)
+    //         })
+
+    //         break;
+    //     case 3:
+
+    //         break;
+    //     case 4:
+
+    //         break;
+    //     default:            
+    // }
+
+
+    
+})
+
+
 
 module.exports = router
