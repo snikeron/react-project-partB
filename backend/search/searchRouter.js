a5a009178257d22b4f4291435c7e5194ce2ba8f0
Ashley <36495929+myashley@users.noreply.github.com>
2018-07-26 14:39:27 +1000

    Add search routes for location and tech

--- backend/search/searchRouter.js
+++ backend/search/searchRouter.js
@@ -19,9 +19,11 @@ router.post('/', (req, res) => {
 })
 
 //// Find Model
-router.post('/salary', (req, res) => {
+router.post('/tech', (req, res) => {
     Candidate.find({
-        minSalary: { $gte: req.body.minSalary },
+        techStack: {
+            $all: req.body.techStack
+        }
         
     })
         .then(results => {
@@ -29,66 +31,13 @@ router.post('/salary', (req, res) => {
         })
 })
 
-router.post('/test', (req, res) => {
-
-    console.log(req.body)
-    console.log(Object.keys(req.body).length)
-    console.log(Object.keys(req.body))
-    console.log(Object.values(req.body))
-    console.log(Object.keys(req.body)[0])
-    console.log(Object.values(req.body)[0])
-
+router.post('/location', (req, res) => {
     Candidate.find({
-        minSalary: { $gte: req.body.minSalary },
-        location: req.body.location,
-     
+        location: req.body.location 
     })
-    .then(results => {
+        .then(results => {
             res.status(200).json(results)
         })
-
-    // switch(Object.keys(req.body).length) {
-    //     case 0:
-            
-    //         break;
-    //     case 1:
-    //         console.log('case1')
-
-    //         const key1 = Object.keys(req.body)[0]
-    //         const value1 = Object.values(req.body)[0]
-
-    //         console.log(key1, value1)
-
-    //         Candidate.find({
-    //             minSalary: req.body.minSalary,
-    //         })
-    //         .then(results => {
-    //             res.status(200).json(results)
-    //         })
-            
-    //         break;
-    //     case 2: 
-
-    //         Candidate.find({
-    //             minSalary: { $gte: req.body.minSalary },
-    //             location: req.body.location,
-    //         })
-    //         .then(results => {
-    //             res.status(200).json(results)
-    //         })
-
-    //         break;
-    //     case 3:
-
-    //         break;
-    //     case 4:
-
-    //         break;
-    //     default:            
-    // }
-
-
-    
 })
 
 
